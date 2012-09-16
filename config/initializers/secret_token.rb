fefcbd524e0dbb3b69a71eba3acfd4e780adb343
Yochay <yochze@gmail.com>
2012-09-16 14:26:20 +0300

    Initializing

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kupid::Application.config.secret_token = '110357e41452372a12f3aac7814499dac93f266712767b66a6645cfcfeda08e0ad0ce22219af7522240496a43520b805978e50fc3be130331a77a1f7d87a7a50'
