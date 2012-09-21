5c3b0fc315f51b9af589574fabfaaa5b0f0902ac
Yochay <yochze@gmail.com>
2012-09-21 19:48:05 +0300

    fixes

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -3,16 +3,11 @@ class MatchesController < ApplicationController
 	def index
 		if user_signed_in?		
 			perfect_match = current_user.raw(current_user.male_list.sample, current_user.female_list.sample)
-		
 			flash[:female] = perfect_match[0]
-				@female = flash[:female]
-		
+			@female = flash[:female]
 			@male = perfect_match[1]
-				flash[:male] = perfect_match[1]
-		
+			flash[:male] = perfect_match[1]
 			@match = Match.new
-			# @match.female = @female
-			# @match.male = @male
 		end
 	end
 
@@ -23,21 +18,21 @@ class MatchesController < ApplicationController
 
 		if params[:yes]
 			@match.result = true
-			User.delay.add_action(current_user.id, "http://www.dudesetmeup.com/matches/158")
 		else
 			@match.result = false
 		end
 		@match.user = current_user
 		@match.save!
-		# if current_user && params[:yes]
-  #     		User.delay.add_action(current_user.id, "http://www.dudesetmeup.com/matches/157")
-  #   	end
+		if current_user && params[:yes]
+      		User.delay.add_action(current_user.id, match_url(@match))
+        end
 
 		redirect_to '/'
 	end
 
 	def show
 		@match = Match.find(params[:id])
+		redirect_to '/' if @match.result == false
 	end
 
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,21 @@
+# encoding: utf-8
+
 module ApplicationHelper
-	# def rand_phrase
-	# 	phrases = []
-	# 	# phrases << "אז מה אם אתה<span style=\"color: #b95959;\">\"In a Relationship\"</span>, זה לא אומר שאתה לא יכול לדאוג לחברים'שך"
-	# 	phrases.push("עזוב אחי אתה לא במצב של לסנן..")
-	# 	phrases.push("מה נשאר לחברים שלך לעשות? רק להתכופף ולתקוף")
-	# 	phrases.push("הוא סבבה והכל אבל.. בלי אבל! סדרי לי אותו!")
-	# 	phrases.push("תגידי.. אממ.. אולי תסדרי לי כבר מישהו?!")
-	# 	# return phrase.sample(1)
-	# end
+	def randomize_index_phrase
+		phrases = []
+		phrases << "אז מה אם אתה <span style=\"color: #b95959;\">\"In a Relationship\"</span>, זה לא אומר שאתה לא יכול לדאוג לחברים'שך"
+		phrases << "עזוב אחי.. תפרגן לו.. הוא <span style=\"color: #b95959;\">לא במצב של לסנן..</span>"
+		phrases << "מה נשאר לחברים שלך לעשות? <span style=\"color: #b95959;\">רק להתכופף ולקטוף</span>"
+		phrases << "הוא סבבה והכל אבל.. <span style=\"color: #b95959;\">בלי אבל!</span> סדרי לי אותו!"
+		phrases << "תגידי.. אממ.. אולי.. אממ.. נו.. אולי.. <span style=\"color: #b95959;\">אולי תסדרי לי כבר מישהו?!</span>"
+		return phrases.sample
+	end
+
+	def randomize_happy_phrase
+		phrases = []
+		phrases << "איזה יופי, שידוך משמים"
+		return phrases.sample
+		
+	end
 end
+
