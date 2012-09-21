85d256faee77ba59035affaac7aaba61f13dbf35
Yochay <yochze@gmail.com>
2012-09-21 11:54:58 +0300

    array.push

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,10 +2,10 @@ module ApplicationHelper
 	def rand_phrase
 		phrases = []
 		# phrases << "אז מה אם אתה<span style=\"color: #b95959;\">\"In a Relationship\"</span>, זה לא אומר שאתה לא יכול לדאוג לחברים'שך"
-		phrases << "עזוב אחי אתה לא במצב של לסנן.."
-		phrases << "מה נשאר לחברים שלך לעשות? רק להתכופף ולתקוף"
-		phrases << "הוא סבבה והכל אבל.. בלי אבל! סדרי לי אותו!"
-		phrases << "תגידי.. אממ.. אולי תסדרי לי כבר מישהו?!"
+		phrases.push("עזוב אחי אתה לא במצב של לסנן..")
+		phrases.push("מה נשאר לחברים שלך לעשות? רק להתכופף ולתקוף")
+		phrases.push("הוא סבבה והכל אבל.. בלי אבל! סדרי לי אותו!")
+		phrases.push("תגידי.. אממ.. אולי תסדרי לי כבר מישהו?!")
 		# return phrase.sample(1)
 	end
 end
