3a6c99fef80a6145c0cd668e11a674673af920ae
Yochay <yochze@gmail.com>
2012-09-21 11:27:42 +0300

    rand phrases

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
-	# def rand_phrase
-	# 	phrases = Array.new
-	# 	phrases << "אז מה אם אתה<span style=\"color: #b95959;\">\"In a Relationship\"</span>, זה לא אומר שאתה לא יכול לדאוג לחברים'שך"
-	# 	return phrase.sample(1)
-	# end
+	def rand_phrase
+		phrases = Array.new
+		phrases << "אז מה אם אתה<span style=\"color: #b95959;\">\"In a Relationship\"</span>, זה לא אומר שאתה לא יכול לדאוג לחברים'שך"
+		return phrase.sample(1)
+	end
 end
